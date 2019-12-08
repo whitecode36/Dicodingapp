66e9f4603de87020aca6937bfeedfdbc31ea5767
whitecode36 <marthintampubolons@gmail.com>
2019-12-08 09:44:25 +0700

    second commmit

--- index.php
+++ index.php
@@ -31,12 +31,21 @@
     $pass = "<Password admin database Anda>";
     $db = "<Nama database Anda>";
 
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:dicodingappserverku.database.windows.net,1433; Database = dicodingdb", "dicoding", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "dicoding", "pwd" => "{your_password_here}", "Database" => "dicodingdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dicodingappserverku.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
     if (isset($_POST['submit'])) {
         try {
